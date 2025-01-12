e11918efb9b12b2dbf461e9f521366a963e2dbd6
Anton Krug <antonkrug@antonkrug.eu>
2025-01-12 16:51:29 +0000

    literals improved

---
+++ utils/literals/all.h
@@ -0,0 +1,9 @@
+//
+// Created by anton on 08/01/2025.
+//
+
+#pragma once
+
+#include "delay.h"
+#include "timer.h"
+#include "usart.h"
--- utils/literals/helper_templates.h
+++ utils/literals/helper_templates.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <limits>
+#include <cassert>
 
 namespace Literals::HelperTemplates {
 
