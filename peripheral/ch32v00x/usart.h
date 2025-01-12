f3b1101e95e5ecb07205238ab62bdd9d3df21180
Anton Krug <antonkrug@antonkrug.eu>
2025-01-12 16:53:00 +0000

    uart supporting enum

--- peripheral/ch32v00x/usart.h
+++ peripheral/ch32v00x/usart.h
@@ -4,12 +4,65 @@
 
 #pragma once
 
-#include <array>
+// #include <array>
 #include <cstdint>
+#include <cmath>
+#include <utility>
 
 // TODO usart baud rates and enum
 
 namespace Peripheral::Usart{
+
+    enum class BaudRate: std::uint32_t {
+        baud4_8k   =   4'800,
+        baud9_6k   =   9'600, // often used - slow baudrate
+        baud19_2k  =  19'200,
+        baud38_4k  =  38'400,
+        baud57_6k  =  57'600,
+        baud115_2k = 115'200, // often used - faster baudrate
+        baud230_4k = 230'400,
+    };
+
+    namespace {
+        // https://medium.com/@mane.tako/understanding-unnamed-namespaces-in-c-7d41d367fd47
+
+        template<std::uint32_t expectedBaudRate, std::uint32_t actualBaudRate, double errorThreshold>
+        constexpr void checkBaudRateError() {
+            // https://ccsinfo.com/forum/viewtopic.php?t=51587
+            constexpr double baudError =
+                fabs(100.0 *
+                     ((static_cast<double>(expectedBaudRate) - actualBaudRate) / expectedBaudRate));
+            static_assert(baudError < errorThreshold, "Bauderror of USARTDIV is over the threashold");
+        }
+
+    }
+
+
+    template<std::uint32_t hclk, std::uint32_t expectedBaudRate, double errorThreshold = 1.0>
+    constexpr static std::uint32_t calculateUsartDivCT() {
+        constexpr double expectedDivider = hclk / (16.0 * expectedBaudRate);
+        static_assert((1<<12) > expectedDivider, "USARTDIV's 12bit mantisa would overflow with these hclk and baudRate values");
+        constexpr std::uint16_t mantisa = static_cast<std::uint16_t>(expectedDivider);
+
+        constexpr double leftOver = std::round(expectedDivider - mantisa);
+        constexpr std::uint8_t fraction = static_cast<std::uint8_t>(leftOver * 16);
+
+        static_assert(fraction > 0 || mantisa >0, "Expected baud rate needs to be 16x slower than HCLK or it would result a zero USARTDIV");
+
+        constexpr std::uint32_t actualBaudRate = hclk / (16.0 * mantisa + fraction);
+        checkBaudRateError<expectedBaudRate, actualBaudRate, errorThreshold>();
+
+        // TODO static print
+        constexpr std::uint32_t usartDiv = mantisa << 4 | fraction;
+        return usartDiv;
+    }
+
+
+    template<std::uint32_t hclk, BaudRate baudRate, double errorThreshold = 1.0>
+    constexpr static std::uint32_t calculateUsartDivCT() {
+        return calculateUsartDivCT<hclk, std::to_underlying(baudRate), errorThreshold>();
+    }
+
 
     #pragma region Declarations
 
