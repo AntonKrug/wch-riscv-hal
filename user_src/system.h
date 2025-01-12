c58ff8719c41aaf2be7be7e8d76f20614f016244
Anton Krug <antonkrug@antonkrug.eu>
2025-01-12 19:21:00 +0000

    Linker script will depend on one main include

---
+++ user_src/system.h
@@ -0,0 +1,12 @@
+//
+// Created by anton on 12/01/2025.
+//
+
+#pragma once
+
+// This file (and its path) is required by linker script, pick up your SOC
+// here and you can include this file in your main etc..
+// This way your files will depend on the same linker scrip and consistently
+// update. Will allow single project serve multiple different SoCs.
+
+#include "system/ch32v003/soc.h"
