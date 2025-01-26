ac1f49d3720db9b74b6c95a3025e0d043167d0a1
Anton Krug <antonkrug@antonkrug.eu>
2025-01-26 13:57:03 +0000

    remove everything just to compile simple startup code first

--- src/user_src/main.cpp
+++ src/user_src/main.cpp
@@ -2,7 +2,7 @@
 
 #define WCH_TARGET_WCH_W32V307
 
-#include <iostream>
+// #include <iostream>
 
 #include "system.h"
 #include "utils/delay.h"
@@ -10,7 +10,7 @@
 #include "firmware_build_info.h"
 #include "system/soc_types.h"
 #include "peripheral/ch32v00x/usart.h"
-#include "system/startup/soc_init.h"
+#include "system/startup/soc_init.c"
 
 using namespace Peripheral;
 
@@ -21,7 +21,7 @@ using namespace Peripheral;
 // NOLINTBEGIN(readability-static-accessed-through-instance)
 int main(int argc, char *argv[]) {
 
-    prepare_system_for_main();
+    // prepare_system_for_main();
 
     // Firmware build info
     // std::cout << "Version: " << firmwareBuildInfo::version << " date: " << firmwareBuildInfo::date << " time: " << firmwareBuildInfo::time << std::endl;
@@ -32,10 +32,10 @@ int main(int argc, char *argv[]) {
     using namespace Literals::Timer;
     using namespace Literals::Usart;
 
-    constexpr auto timer = 100_ms_to_hz;
-    std::cout << timer << std::endl;
+    // constexpr auto timer = 100_ms_to_hz;
+    // std::cout << timer << std::endl;
     Utils::delayMs(1.5_hour_to_ms);
-    constexpr auto timerFrequencySetting = 20_ns_to_hz;
+    // constexpr auto timerFrequencySetting = 20_ns_to_hz;
     // auto something = Soc::Usart::Channel1Mapping0.device;
 
     // UART div calculations
@@ -45,38 +45,42 @@ int main(int argc, char *argv[]) {
     // constexpr auto time = 1_hour_to_ms + 30_min_to_ms;
 
     // UART - This should pass
-    constexpr auto div = Usart::calculateUsartDivCT<11.0592_mhz_to_hz, 9.6_kbaud>();
-
-    Peripheral::Usart::Device<SoC::PeripheralAddreses::usart1> device2;
-    // std::cout << "Uart base address " << device2.baseAddressUint32 << std::endl;
-
-    Rcc::PeripheralBus2Reset a {
-        .ioAuxiliary = Rcc::Reset::reset,
-    };
-
-    Rcc::PeripheralBus2ClockEnable b {
-        .portA = Rcc::ModuleClock::on,
-    };
+    // constexpr auto div = Usart::calculateUsartDivCT<11.0592_mhz_to_hz, 9.6_kbaud>();
+    //
+    // Peripheral::Usart::Device<SoC::PeripheralAddreses::usart1> device2;
+    // // std::cout << "Uart base address " << device2.baseAddressUint32 << std::endl;
+    //
+    // Rcc::PeripheralBus2Reset a {
+    //     .ioAuxiliary = Rcc::Reset::reset,
+    // };
+    //
+    // Rcc::PeripheralBus2ClockEnable b {
+    //     .portA = Rcc::ModuleClock::on,
+    // };
 
 
     // GPIO experiments
-    constexpr auto tdi  = Soc::Gpio.A.GetPin(0);
-    constexpr auto tms  = Soc::Gpio.A.GetPin(1);
-    constexpr auto tdo  = Soc::Gpio.A.GetPin(2);
-    constexpr auto data = Soc::Gpio.C.GetPin({ 2, 4, 5, 7 });
-    // data = argc;
-
-    // auto var = tdi.mode<Driver::Gpio::PinMode::outputPushPull>().on<tdi>().commitExact();
-    Gpio::PinsSet<tdi, tdo, tms>() = 11;
-
-    constexpr Gpio::Sequence<> changes;
-    constexpr auto ans = changes.mode<tdi, Gpio::PinMode::inputPullUpOrDown>().off<tdo>().on<tdi>().on<data>();
+    // constexpr auto tdi  = Soc::Gpio.A.GetPin(0);
+    // constexpr auto tms  = Soc::Gpio.A.GetPin(1);
+    // constexpr auto tdo  = Soc::Gpio.A.GetPin(2);
+    // constexpr auto data = Soc::Gpio.C.GetPin({ 2, 4, 5, 7 });
+    // // data = argc;
+    //
+    // // auto var = tdi.mode<Driver::Gpio::PinMode::outputPushPull>().on<tdi>().commitExact();
+    // Gpio::PinsSet<tdi, tdo, tms>() = 11;
+    //
+    // constexpr Gpio::Sequence<> changes;
+    // constexpr auto ans = changes.mode<tdi, Gpio::PinMode::inputPullUpOrDown>().off<tdo>().on<tdi>().on<data>();
 
     //changes.on<tdi>().commitExact();
     //constexpr auto ans = changes.on<tdi>();
     //return tdi.mode<Driver::Gpio::PinMode::outputOpenDrain>().on<tdi>().commitExact();
     //constexpr auto b = tdi.actuate().on<tdo>().commitExact();
 
-    return ans.executeExact();
+    // return ans.executeExact();
+
+    while (1) {
+
+    }
 }
 // NOLINTEND(readability-static-accessed-through-instance)
