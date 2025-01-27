5e5520a0bc3e7efc2dcc78b6c3d6aa34a6da8ceb
Anton Krug <antonkrug@antonkrug.eu>
2025-01-27 21:14:05 +0000

    better name and correct namespace

--- src/system/memory_map/concepts.h
+++ src/system/memory_map/concepts.h
@@ -4,15 +4,19 @@
 
 #pragma once
 
-#include <concepts> // https://en.cppreference.com/w/cpp/language/constraints
+// https://en.cppreference.com/w/cpp/language/constraints
+#include <concepts>
+
+namespace SoC::MemoryConcepts {
+
+    template<long long int address>
+    concept IsValidPeripheralBaseAddress = (
+        address >= 0x4000'0000 &&
+        address <= 0x5005'0400 &&
+        (address % 0x400) == 0);  // Each periperal is aligned to 1k (0x400)
 
 
-template<long long int address>
-concept ValidPeripheralBaseAddress = (
-    address >= 0x4000'0000 &&
-    address <= 0x5005'0400 &&
-    (address % 0x400) == 0);  // Each periperal is aligned to 1k (0x400)
+    template<long long int address>
+    concept IsWordAlignedBaseAddress = ((address % 0x4) == 0);
 
-
-template<long long int address>
-concept WordAlignedBaseAddress = ((address % 0x4) == 0);
+}
