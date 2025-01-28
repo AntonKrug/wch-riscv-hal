26e31e22dc71df01eaaca885dac8986e5e204951
Anton Krug <antonkrug@antonkrug.eu>
2025-01-28 22:28:58 +0000

    more generic and cleaner csr approach

--- src/system/riscv/csr_access.h
+++ src/system/riscv/csr_access.h
@@ -13,15 +13,24 @@ namespace Riscv::Csr {
 
 
     template<auto Csr>
-    constexpr auto toAddress() -> std::uint16_t requires Riscv::Concepts::IsCsrEnum<Csr> {
+    constexpr auto toAddress() -> std::uint16_t requires Riscv::Concepts::IsCsrEnumType<Csr> {
         return static_cast<std::uint16_t>(Csr);
     }
 
-    //https://developer.arm.com/documentation/100748/0613/using-assembly-and-intrinsics-in-c-or-c---code/writing-inline-assembly-code
 
+    template<auto Enum>
+    constexpr auto toValue() -> std::uint16_t requires Riscv::Concepts::IsCsrMaskedEnums<Enum> {
+        return static_cast<std::uint16_t>(Enum);
+    }
+
+
+    //https://developer.arm.com/documentation/100748/0613/using-assembly-and-intrinsics-in-c-or-c---code/writing-inline-assembly-code
     template <auto Csr>
-    requires Riscv::Concepts::IsCsrValid<Csr>
-    constexpr auto readCsr() -> std::uint32_t {
+    requires Riscv::Concepts::IsCsrEnumValid<Csr>
+    inline
+    constexpr auto
+    __attribute__ ((always_inline))
+    readCsr() -> std::uint32_t {
         std::uint32_t result;
 
         __asm__ volatile(
@@ -32,9 +41,14 @@ namespace Riscv::Csr {
         return result;
     }
 
+    // All the writes for various types
     template <auto Csr, auto value>
-    requires Riscv::Concepts::IsCsrValid<Csr>
-    constexpr auto writeCsr() -> void {
+    requires Riscv::Concepts::IsCsrEnumValid<Csr> &&
+             Riscv::Concepts::IsCsrValueCorrectType<value>
+    inline
+    constexpr auto
+    __attribute__ ((always_inline))
+    writeCsr() -> void {
         __asm__ volatile(
             "csrw %0, %1"
             : // no output
@@ -42,29 +56,68 @@ namespace Riscv::Csr {
         );
     }
 
-    template <auto Csr>
-    requires Riscv::Concepts::IsCsrValid<Csr>
-    constexpr auto writeCsr(const unsigned int value) -> void {
+
+    // For zeroing writes
+    template <auto Csr, auto value>
+    requires Riscv::Concepts::IsCsrEnumValid<Csr> &&
+             Riscv::Concepts::IsCsrValueCorrectType<value> &&
+             Riscv::Concepts::IsCsrValueZero<value>
+    inline
+    constexpr auto
+    __attribute__ ((always_inline))
+    writeCsr() -> void {
         __asm__ volatile(
-            "csrw %0, %1"
+            "csrw %0, x0"
             : // no output
-            : "i"(static_cast<std::uint16_t>(Csr)), "r"(value)
+            : "i"(static_cast<std::uint16_t>(Csr))
         );
     }
 
-    template <auto Csr>
-    requires Riscv::Concepts::IsCsrValid<Csr>
-    constexpr auto writeCsr(const std::uint32_t value) -> void {
-        __asm__ volatile(
-            "csrw %0, %1"
-            : // no output
-            : "i"(static_cast<std::uint16_t>(Csr)), "r"(value)
-        );
-    }
+    // template <auto Csr, auto value>
+    // requires Riscv::Concepts::IsCsrEnumValid<Csr>
+    // inline
+    // constexpr auto
+    // __attribute__ ((always_inline))
+    // writeCsr() -> void {
+    //     __asm__ volatile(
+    //         "csrw %0, %1"
+    //         : // no output
+    //         : "i"(static_cast<std::uint16_t>(Csr)), "r"(value)
+    //     );
+    // }
+    //
+    // template <auto Csr>
+    // requires Riscv::Concepts::IsCsrEnumValid<Csr>
+    // inline
+    // constexpr auto
+    // __attribute__ ((always_inline))
+    // writeCsr(const unsigned int value) -> void {
+    //     __asm__ volatile(
+    //         "csrw %0, %1"
+    //         : // no output
+    //         : "i"(static_cast<std::uint16_t>(Csr)), "r"(value)
+    //     );
+    // }
+    //
+    // template <auto Csr>
+    // requires Riscv::Concepts::IsCsrEnumValid<Csr>
+    // inline
+    // constexpr auto
+    // __attribute__ ((always_inline))
+    // writeCsr(const std::uint32_t value) -> void {
+    //     __asm__ volatile(
+    //         "csrw %0, %1"
+    //         : // no output
+    //         : "i"(static_cast<std::uint16_t>(Csr)), "r"(value)
+    //     );
+    // }
 
     template <auto Csr>
-    requires Riscv::Concepts::IsCsrValid<Csr>
-    constexpr auto writeCsr(const std::uint16_t value) -> void {
+    requires Riscv::Concepts::IsCsrEnumValid<Csr>
+    inline
+    constexpr auto
+    __attribute__ ((always_inline))
+    writeCsr(const std::uint16_t value) -> void {
         __asm__ volatile(
             "csrw %0, %1"
             : // no output
