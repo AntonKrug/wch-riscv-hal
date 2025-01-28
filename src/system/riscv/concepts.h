d597675d0952d1bc8e31616397e09cbe5052b7ad
Anton Krug <antonkrug@antonkrug.eu>
2025-01-28 22:22:46 +0000

    cleanup unecesary decltype wrappers

--- src/system/riscv/concepts.h
+++ src/system/riscv/concepts.h
@@ -11,45 +11,61 @@
 
 namespace Riscv::Concepts {
 
+    template<auto Mask>
+    concept IsCsrMaskedEnums =
+        std::is_same_v<decltype(Mask), Riscv::Csr::Priviledge> ||
+        std::is_same_v<decltype(Mask), Riscv::Csr::ReadWrite>;
 
-    template<typename CsrType>
+
+    template<auto CsrEnum>
     concept IsCsrEnumType =
-        std::is_same_v<CsrType, Riscv::Csr::CsrQingKeV2> ||
-        std::is_same_v<CsrType, Riscv::Csr::CsrQingKeV3> ||
-        std::is_same_v<CsrType, Riscv::Csr::CsrQingKeV4>;
+        std::is_same_v<decltype(CsrEnum), Riscv::Csr::QingKeV2> ||
+        std::is_same_v<decltype(CsrEnum), Riscv::Csr::QingKeV3> ||
+        std::is_same_v<decltype(CsrEnum), Riscv::Csr::QingKeV4>;
+
+
+    template<auto CsrValueType>
+    concept IsCsrValueCorrectType =
+        std::is_same_v<decltype(CsrValueType), int>                ||
+        std::is_same_v<decltype(CsrValueType), unsigned int>       ||
+        std::is_same_v<decltype(CsrValueType), std::uint32_t>      ||
+        std::is_same_v<decltype(CsrValueType), std::uint16_t>      ||
+        std::is_same_v<decltype(CsrValueType), std::uint8_t>;
+
+
+    template<auto CsrValueType>
+    concept IsCsrValueZero = CsrValueType == 0u;
+
+
+    template<auto csrAddress>
+    concept IsCsrValidAddressRange = (csrAddress >= 0 && csrAddress < (1u<<12));
+
 
     template<auto Csr>
-    concept IsCsrEnum = IsCsrEnumType<decltype(Csr)>;
-
-    template<long long int csrAddress>
-    concept IsCsrValidAddress = (csrAddress >= 0 && csrAddress < (1u<<12));
-
-
-    template<auto Csr>
-    concept IsCsrValid =
-        IsCsrEnum<Csr> &&
-        IsCsrValidAddress<static_cast<std::uint16_t>(Csr)>;
+    concept IsCsrEnumValid =
+        IsCsrEnumType<Csr> &&
+        IsCsrValidAddressRange<static_cast<std::uint16_t>(Csr)>;
 
 
-    template<long long int CsrAddress>
+    template<auto CsrAddress>
     concept IsCsrWritableAddress = (
-        (CsrAddress  & Riscv::Csr::readWriteMask) != static_cast<std::uint16_t>(Riscv::Csr::ReadWrite::readOnly) );
+        (CsrAddress  & Riscv::Csr::maskReadWrite) != static_cast<std::uint16_t>(Riscv::Csr::ReadWrite::readOnly) );
 
 
     template<auto Csr>
     concept IsCsrWritable =
-            IsCsrEnumType<decltype(Csr)> &&
-            IsCsrWritableAddress<static_cast<std::uint16_t>(Csr)>;
+        IsCsrEnumType<Csr> &&
+        IsCsrWritableAddress<static_cast<std::uint16_t>(Csr)>;
 
 
-    template<long long int CsrAddress>
+    template<auto CsrAddress>
     concept IsCsrMachinePriviledgeAddress = (
-        (CsrAddress  & Riscv::Csr::priviledgeMask) == static_cast<std::uint16_t>(Riscv::Csr::Priviledge::machine) );
+        (CsrAddress  & Riscv::Csr::maskPriviledge) == static_cast<std::uint16_t>(Riscv::Csr::Priviledge::machine) );
 
 
     template<auto Csr>
     concept IsCsrMachinePriviledge =
-        IsCsrEnumType<decltype(Csr)> &&
+        IsCsrEnumType<Csr> &&
         IsCsrMachinePriviledgeAddress<static_cast<std::uint16_t>(Csr)>;
 
 
