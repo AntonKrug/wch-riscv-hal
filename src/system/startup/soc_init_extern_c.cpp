2c5560a2b07e19d8e552388583a24aaec6e8efd0
Anton Krug <antonkrug@antonkrug.eu>
2025-01-28 22:25:45 +0000

    c++ system init

---
+++ src/system/startup/soc_init_extern_c.cpp
@@ -0,0 +1,99 @@
+//
+// Created by anton on 28/01/2025.
+//
+
+#include "generated_startup_configuration.h"
+//#include "soc_init_for_c.h"
+#include "system/riscv/csr_access.h"
+#include "system/riscv/csr_register/intsyscr.h"
+
+extern "C" {
+    // DATA section
+    extern unsigned int __data_rom_start;
+    extern unsigned int __data_rom_end;
+    extern unsigned int __data_ram_start;
+    extern unsigned int __data_ram_end;
+
+
+    // BSS section
+    extern unsigned int __block_started_by_symbol;
+    extern unsigned int __block_started_by_symbol_end;
+
+
+    // https://gcc.gnu.org/onlinedocs/gcc-4.0.1/gcc/Optimize-Options.html
+    inline
+    void
+    __attribute__ ((
+        always_inline,
+        optimize("-Os"),
+        optimize("-fno-strict-aliasing"),
+        optimize("-fomit-frame-pointer"),
+        optimize("-fexpensive-optimizations"),
+        optimize("-fdefer-pop"),
+        optimize("-fdelete-null-pointer-checks"),
+        optimize("-fcaller-saves"),
+        optimize("-finline-functions"),
+        optimize("-fpeephole"),
+        optimize("-fpeephole2"),
+        optimize("-fschedule-insns"),
+        optimize("-fschedule-insns2")
+        ))
+    prepare_system_for_main(void) {
+        // Data ROM -> RAM copy
+        #ifndef WCH_STARTUP_SKIP_DATA_SECTION_COPY
+            // Get the address locations from the linker script
+            //const unsigned int* rom_end = &__data_rom_end;
+            const unsigned int* data_ram_end = &__data_ram_end;
+            const unsigned int* data_rom_ptr = &__data_rom_start;
+            unsigned int*       data_ram_ptr = &__data_ram_start;
+
+            // Copy data from ROM to RAM, word (4 bytes) at a time
+            do {
+                *data_ram_ptr++ = *data_rom_ptr++;
+            } while (data_ram_ptr < data_ram_end);
+        #endif
+
+        // BSS RAM zeroizing
+        #ifndef WCH_STARTUP_SKIP_BSS_SECTION_ZEROIZING
+            // Get the address locations from the linker script
+            const unsigned int* zero_ram_end = &__block_started_by_symbol_end;
+            unsigned int*       zero_ram_ptr = &__block_started_by_symbol;
+
+            // Zero data in RAM, word (4 bytes) at a time
+            do {
+                *zero_ram_ptr++ = 0;
+            } while (zero_ram_ptr < zero_ram_end);
+        #endif
+
+        Riscv::Csr::readCsr<Riscv::Csr::QingKeV2::intsyscr>();
+        Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr, 0>();
+        Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr, 0x20000000>();
+        Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr, 0xffffff>();
+
+        constexpr auto interuptSettings = Riscv::Csr::Intsyscr::combine(
+            Riscv::Csr::Intsyscr::Hwstken::hpeEnable,
+            Riscv::Csr::Intsyscr::Inesten::interuptNestingEnable,
+            Riscv::Csr::Intsyscr::Eabien::eabiDisable);
+
+        Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr, interuptSettings>();
+        // riscv_qingke2_write_intsyscr(0x10);
+        // Riscv::Csr::readCSR<>()
+    }
+
+    void
+    // inline
+    __attribute__((
+        // always_inline,
+        section(".text.handle_reset"),
+        naked,
+        optimize("-O1"))) handle_reset() {
+        asm volatile (
+            ".option norelax\n"           // Disable relaxation for the next instruction
+            "la gp, __global_pointer$\n"  // Load the address of the global pointer
+            ".option relax\n"             // Enable relaxation again
+            "la sp, __stack_end\n"        // Load the address of the stack pointer
+        );
+        prepare_system_for_main();
+    }
+}
+
