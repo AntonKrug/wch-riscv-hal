d7edf4b6648dbd5e9f61942dcb595d79ce539b0e
Anton Krug <antonkrug@antonkrug.eu>
2025-01-28 22:24:19 +0000

    not going to use the wrappers

--- src/system/riscv/csr_access_extern_c.cpp
+++ src/system/riscv/csr_access_extern_c.cpp
@@ -4,23 +4,23 @@
 
 // #pragma once
 
-// #include "csr_access_c.h"
+// #include "csr_access_for_c.h"
 #include "csr_access.h"
 #include "csr.h"
 
-template auto Riscv::Csr::readCsr<Riscv::Csr::CsrQingKeV2::intsyscr>() -> std::uint32_t;
-template auto Riscv::Csr::writeCsr<Riscv::Csr::CsrQingKeV2::intsyscr>(std::uint32_t value) -> void;
-template auto Riscv::Csr::writeCsr<Riscv::Csr::CsrQingKeV2::intsyscr>(const unsigned int value) -> void;
+// template auto Riscv::Csr::readCsr<Riscv::Csr::QingKeV2::intsyscr>() -> std::uint32_t;
+// template auto Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr>(std::uint32_t value) -> void;
+// template auto Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr>(const unsigned int value) -> void;
 
 
 extern "C" {
 
     __attribute__((always_inline)) inline int riscv_qingke2_read_intsyscr() {
-        return Riscv::Csr::readCsr<Riscv::Csr::CsrQingKeV2::intsyscr>();
+        return Riscv::Csr::readCsr<Riscv::Csr::QingKeV2::intsyscr>();
     }
 
     __attribute__((always_inline)) inline void riscv_qingke2_write_intsyscr(const unsigned int value) {
-        return Riscv::Csr::writeCsr<Riscv::Csr::CsrQingKeV2::intsyscr>(value);
+        return Riscv::Csr::writeCsr<Riscv::Csr::QingKeV2::intsyscr>(value);
     }
 
 }
