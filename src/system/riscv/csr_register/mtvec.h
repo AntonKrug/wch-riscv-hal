0a19d02addf1b37fca07d8d8f3ae88631a4a1579
Anton Krug <antonkrug@antonkrug.eu>
2025-01-30 19:15:33 +0000

    Added safety check for address range

--- src/system/riscv/csr_register/mtvec.h
+++ src/system/riscv/csr_register/mtvec.h
@@ -3,7 +3,7 @@
 //
 
 // https://www.wch-ic.com/downloads/QingKeV2_Processor_Manual_PDF.html
-// NOTE: the WCH implemetion differs tiny bit from the standard spec
+// NOTE: the WCH implemetion differs tiny bit from the standard spec as extra field
 
 #pragma once
 
@@ -31,6 +31,7 @@ namespace Riscv::Csr::Mtvec {
 
 
     enum class BaseAddr: std::uint32_t {
+        // the value needs to be 1k aligned (0x400)
         fieldBitMask = 0b111111111111111111111111111111'0'0,
     };
 
@@ -40,6 +41,19 @@ namespace Riscv::Csr::Mtvec {
         std::is_same_v<Field, Mode0> ||
         std::is_same_v<Field, Mode1> ||
         std::is_same_v<Field, BaseAddr>;
+
+
+    template<auto Address>
+    constexpr std::uint32_t CheckVectorBaseAddressAlignment() {
+        static_assert(Address >= 0,         "Address must be positive number.");
+        static_assert(Address % 0x400 == 0, "Address must be aligned to 1K boundary (0x400).");
+        static_assert(
+            ((Address >= 0)           && (Address < 0x78000))     || // in flash ROM 480kibi range
+            ((Address >= 0x2000'0000) && (Address < 0x2004'0000))    // in sram  RAM 256kibi range
+            , "Address must be inside ROM or RAM address range");
+
+        return static_cast<std::uint32_t>(Address);
+    }
 
 
 }
