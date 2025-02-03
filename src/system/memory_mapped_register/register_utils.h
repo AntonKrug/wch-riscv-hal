dadea07f7e9a243bd71625e83451bf6a092dd3af
Anton Krug <antonkrug@antonkrug.eu>
2025-02-03 23:47:00 +0000

    Raw normalization and enum casting

--- src/system/memory_mapped_register/register_utils.h
+++ src/system/memory_mapped_register/register_utils.h
@@ -8,11 +8,11 @@
 
 #include "concepts.h"
 
-namespace SoC::MemoryMappedRegister {
+namespace SoC::MemMappedReg {
 
 
     template<std::uint32_t bitMaskValue>
-    constexpr std::uint8_t bitMasksffsetCt() {
+    constexpr std::uint8_t bitMaskOffsetCt() {
         std::uint8_t count = 0;
         std::uint32_t tmp = bitMaskValue;
         while ( ((tmp&0b1) == 0) && (count<31) ) {
@@ -40,12 +40,24 @@ namespace SoC::MemoryMappedRegister {
     }
 
 
-    template<
-        SoC::MemoryMappedRegister::Concepts::RegisterFieldEnumWhichContainsFieldBitMask RegisterFieldType,
-        std::uint32_t registerValue>
-    constexpr auto getEnumFromValue() {
-        return static_cast<RegisterFieldType>(registerValue);
+    template<Concept::RegisterFieldEnumWhichContainsFieldBitMask RegisterFieldType>
+    inline constexpr auto
+    __attribute__ ((always_inline))
+    rawToEnum(std::uint32_t registerValue) {
+        constexpr auto mask = static_cast<std::uint32_t>(RegisterFieldType::fieldBitMask);
+        return static_cast<RegisterFieldType>(mask & registerValue);
     }
+
+
+    template<Concept::RegisterFieldEnumWhichContainsFieldBitMask RegisterFieldType>
+    inline constexpr auto
+    __attribute__ ((always_inline))
+    rawToNormalized(std::uint32_t registerValue) -> std::uint32_t {
+        constexpr auto mask = static_cast<std::uint32_t>(RegisterFieldType::fieldBitMask);
+        constexpr auto offset = bitMaskOffsetCt<static_cast<std::uint32_t>(RegisterFieldType::fieldBitMask)>();
+        return (registerValue & mask) >> offset;
+    }
+
 
 
 
