b80bf3384ed996ac3106287954c54e9593281b2c
Anton Krug <antonkrug@antonkrug.eu>
2025-02-04 21:44:23 +0000

    Renaming the RCC's fields

--- src/peripheral/ch32v00x/rcc/ctlr.h
+++ src/peripheral/ch32v00x/rcc/ctlr.h
@@ -7,33 +7,33 @@
 #include <cstdint>
 #include <type_traits>
 
-namespace Peripheral::Rcc {
+namespace Peripheral::Rcc::Ctlr {
 
     // TODO: check other cpus
-    enum class HsionInternalHighSpeedClockEnable: std::uint32_t {
+    enum class HSION_InternalHighSpeedClockEnable: std::uint32_t {
         fieldBitMask = 0b1, // not holding any settings or value, it's a bitmask for this specific field
-        enable24MHz  = 0b1, // HSI can set by HW as well (when waking up from standby or when expierencing failure while using HSE)
+        enable       = 0b1, // HSI can set by HW as well (when waking up from standby or when expierencing failure while using HSE). v003=24Mhz, x033/x035=48Mhz others=8Mhz
         disable      = 0    // after disabling HSI, it takes 6 cycles to propagate the change
     };
 
 
-    enum class HsirdyInternalHighSpeedClockStable: std::uint32_t {
+    enum class HSIRDY_InternalHighSpeedClockStable: std::uint32_t {
         fieldBitMask     = 0b1'0, // not holding any settings or value, it's a bitmask for this specific field
-        isReadyAndStable = 0b1'0, // HSI can set by HW as well (when waking up from standby or when expierencing failure while using HSE)
+        isStableAndReady = 0b1'0, // HSI can set by HW as well (when waking up from standby or when expierencing failure while using HSE)
         notReady         = 0      // HSI not stable enough yet, wait for longer
     };
 
 
-    enum class HsitrimInternalHighSpeedTrim: std::uint32_t {
+    enum class HSITRIM_InternalHighSpeedTrim: std::uint32_t {
         // Able to superimpose on top of HSICAL[7:0] to adjust HSI frequency. Range is 0-31 60kHz steps, 16 is the default midpoint
         fieldBitMask     = 0b11111'0'0'0, // not holding any settings or value, it's a bitmask for this specific field
     };
 
 
-    template<typename RegisterField>
-    concept IsAnyRegisterField =
-        std::is_same_v<RegisterField, HsionInternalHighSpeedClockEnable> ||
-        std::is_same_v<RegisterField, HsirdyInternalHighSpeedClockStable>
-    ;
+    template<typename RegField>
+    concept IsAnyRegField =
+        std::is_same_v<RegField, HSION_InternalHighSpeedClockEnable> ||
+        std::is_same_v<RegField, HSIRDY_InternalHighSpeedClockStable> ||
+        std::is_same_v<RegField, HSITRIM_InternalHighSpeedTrim>;
 
 }
