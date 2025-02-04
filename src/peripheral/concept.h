bead8b9567a12a2c769bbc253d4fd9fd950c8e03
Anton Krug <antonkrug@antonkrug.eu>
2025-02-04 21:44:56 +0000

    concepts for peripherals

---
+++ src/peripheral/concept.h
@@ -0,0 +1,18 @@
+//
+// Created by anton on 04/02/2025.
+//
+
+#pragma once
+
+#include <cstdint>
+
+#include "ch32v00x/rcc/ctlr.h"
+
+// template<typename PeripheralInstance>
+// concept PeripheralHasBaseAddress = requires
+// { { PeripheralInstance::baseAddr }; };
+
+
+
+// template <typename T>
+// concept PeripheralHasBaseAddr = decltype(T::baseAddr, std::true_type{})::value;
