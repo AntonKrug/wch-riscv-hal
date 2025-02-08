584123513c196e8a4b97990bd7e38fe3115289fb
Anton Krug <antonkrug@antonkrug.eu>
2025-02-08 12:34:48 +0000

    memory mapped registers do not need it when it's in the register

--- src/system/memory_mapped_register/register_utils.h
+++ src/system/memory_mapped_register/register_utils.h
@@ -7,37 +7,9 @@
 #include <cstdint>
 
 #include "concepts.h"
+#include "system/register/utils.h"
 
 namespace SoC::MemMappedReg {
-
-
-    template<std::uint32_t bitMaskValue>
-    constexpr std::uint8_t bitMaskOffsetCt() {
-        std::uint8_t count = 0;
-        std::uint32_t tmp = bitMaskValue;
-        while ( ((tmp&0b1) == 0) && (count<31) ) {
-            tmp >>= 1;
-            count++;
-        }
-        return count;
-    }
-
-
-    template<std::uint32_t bitMaskValue>
-    constexpr std::uint8_t bitMasksSizeCt() {
-        std::uint8_t count = 0;
-        std::uint32_t tmp = bitMaskValue;
-        while ( ((tmp&0b1) == 0) && (count<31) ) {
-            tmp >>= 1;
-            count++;
-        }
-        count = 0;
-        while (tmp&0b1) {
-            tmp >>= 1;
-            count++;
-        }
-        return count;
-    }
 
 
     template<Concept::RegisterFieldEnumWhichContainsFieldBitMask RegisterFieldType>
@@ -54,7 +26,7 @@ namespace SoC::MemMappedReg {
     __attribute__ ((always_inline))
     rawToNormalized(const std::uint32_t registerValue) -> std::uint32_t {
         constexpr auto mask = static_cast<std::uint32_t>(RegisterFieldType::fieldBitMask);
-        constexpr auto offset = bitMaskOffsetCt<static_cast<std::uint32_t>(RegisterFieldType::fieldBitMask)>();
+        constexpr auto offset = SoC::Reg::bitMaskOffsetCt<static_cast<std::uint32_t>(RegisterFieldType::fieldBitMask)>();
         return (registerValue & mask) >> offset;
     }
 
