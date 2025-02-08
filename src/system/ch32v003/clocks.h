91c20e3f62de6bf940b3ae5fcf403fbe3ff46315
Anton Krug <antonkrug@antonkrug.eu>
2025-02-08 12:35:39 +0000

    Clocks are shared between sub packages

---
+++ src/system/ch32v003/clocks.h
@@ -0,0 +1,15 @@
+//
+// Created by anton on 08/02/2025.
+//
+
+#pragma once
+
+#include <cstdint>
+
+#include "utils/literals/timer.h"
+
+namespace Soc::Clocks {
+    using namespace Literals::Timer;
+    constexpr std::uint32_t Hsi = 24_mhz_to_hz;  // High speed internal RC oscilator v003=24Mhz, x033/x035=48Mhz others=8Mhz
+    constexpr std::uint32_t Lsi = 128_khz_to_hz; // for time, sleep modes, watchdog
+}
