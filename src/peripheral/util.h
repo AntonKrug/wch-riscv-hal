b6612d32d2a4033c560985b86229298e18c8323e
Anton Krug <antonkrug@antonkrug.eu>
2025-02-12 19:06:19 +0000

    Adding "isSameAsField" which can be driven from tuple. And cleaned signatures

--- src/peripheral/util.h
+++ src/peripheral/util.h
@@ -7,32 +7,51 @@
 #include <utility>
 #include <cstdint>
 
-#include "register_field_to_register.h"
-#include "system/register/util.h"
+// #include "register_field_to_register.h"
+// #include "system/register/util.h"
 //#include <cstddef>
 
 namespace Peripheral::Util {
 
-    #pragma region Bitmask
+    #pragma region Field Bitmask and Field Access
 
-    template<typename f, std::size_t Index>
-    constexpr std::uint32_t getMaskFromTupleIndex() {
-        return static_cast<std::uint32_t>(std::tuple_element_t<Index, f>::fieldBitMask);
+    template<typename Tuple, std::size_t Index>
+    constexpr auto getBitmaskFromTupleIndex() -> std::uint32_t {
+        return static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldBitMask);
     }
 
-    template<typename f, std::size_t Index>
-    constexpr bool isWritableFromTupleIndex() {
-        return Soc::Reg::isFieldAccessWritable<static_cast<std::uint32_t>(std::tuple_element_t<Index, f>::fieldAccess)>();
+    template<typename Tuple, std::size_t Index>
+    constexpr auto getWritableFromTupleIndex() -> bool {
+        return Soc::Reg::isFieldAccessWritable<static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldAccess)>();
     }
 
-    template<typename f, std::size_t... Indices>
-    constexpr std::uint32_t getWritableMaskFromTupleIndices(std::index_sequence<Indices...>) {
-        return ( (isWritableFromTupleIndex?getMaskFromTupleIndex<f, Indices>():0) | ...);
+    template<typename Tuple, std::size_t... Indices>
+    constexpr auto getWritableMaskFromTupleIndices(std::index_sequence<Indices...>) -> std::uint32_t {
+        return ( (getWritableFromTupleIndex<Tuple, Indices>?getBitmaskFromTupleIndex<Tuple, Indices>():0) | ...);
     }
 
-    template<typename f>
-    constexpr std::uint32_t getWritableMaskFromTuple() {
-        return getMasksFromTupleIndices<f>(std::make_index_sequence<std::tuple_size_v<f>>{});
+    template<typename Tuple>
+    constexpr auto getWritableMaskFromTuple() -> std::uint32_t {
+        return getMasksFromTupleIndices<Tuple>(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+    }
+
+    #pragma endregion
+
+    #pragma region IsSameAsOneFieldFromTuple
+
+    template<typename RegField, typename Tuple, std::size_t Index>
+    constexpr auto IsSameAsOneFieldFromTupleIndex() -> bool {
+        return std::is_same_v<RegField, std::tuple_element_t<Index, Tuple>>;
+    }
+
+    template<typename RegField, typename Tuple, std::size_t... Indices>
+    constexpr auto IsSameAsOneFieldFromTupleIndices(std::index_sequence<Indices...>) -> bool {
+        return ( IsSameAsOneFieldFromTupleIndex<RegField, Tuple, Indices>() || ...);
+    }
+
+    template<typename RegField, typename Tuple>
+    constexpr bool IsSameAsOneFieldFromTuple() {
+        return IsSameAsOneFieldFromTupleIndices<RegField, Tuple>(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
     }
 
     #pragma endregion
