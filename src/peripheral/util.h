35c941681659578b834a295aaf665d2a77f87742
Anton Krug <antonkrug@antonkrug.eu>
2025-02-12 19:31:54 +0000

    Move from peripheral utilities to register utilities

--- src/peripheral/util.h
+++ src/peripheral/util.h
@@ -4,57 +4,8 @@
 
 #pragma once
 
-#include <utility>
-#include <cstdint>
-
-// #include "register_field_to_register.h"
-// #include "system/register/util.h"
-//#include <cstddef>
-
 namespace Peripheral::Util {
 
-    #pragma region Field Bitmask and Field Access
-
-    template<typename Tuple, std::size_t Index>
-    constexpr auto getBitmaskFromTupleIndex() -> std::uint32_t {
-        return static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldBitMask);
-    }
-
-    template<typename Tuple, std::size_t Index>
-    constexpr auto getWritableFromTupleIndex() -> bool {
-        return Soc::Reg::isFieldAccessWritable<static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldAccess)>();
-    }
-
-    template<typename Tuple, std::size_t... Indices>
-    constexpr auto getWritableMaskFromTupleIndices(std::index_sequence<Indices...>) -> std::uint32_t {
-        return ( (getWritableFromTupleIndex<Tuple, Indices>?getBitmaskFromTupleIndex<Tuple, Indices>():0) | ...);
-    }
-
-    template<typename Tuple>
-    constexpr auto getWritableMaskFromTuple() -> std::uint32_t {
-        return getMasksFromTupleIndices<Tuple>(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
-    }
-
-    #pragma endregion
-
-    #pragma region IsSameAsOneFieldFromTuple
-
-    template<typename RegField, typename Tuple, std::size_t Index>
-    constexpr auto IsSameAsOneFieldFromTupleIndex() -> bool {
-        return std::is_same_v<RegField, std::tuple_element_t<Index, Tuple>>;
-    }
-
-    template<typename RegField, typename Tuple, std::size_t... Indices>
-    constexpr auto IsSameAsOneFieldFromTupleIndices(std::index_sequence<Indices...>) -> bool {
-        return ( IsSameAsOneFieldFromTupleIndex<RegField, Tuple, Indices>() || ...);
-    }
-
-    template<typename RegField, typename Tuple>
-    constexpr bool IsSameAsOneFieldFromTuple() {
-        return IsSameAsOneFieldFromTupleIndices<RegField, Tuple>(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
-    }
-
-    #pragma endregion
 
 }
 
--- src/system/register/util.h
+++ src/system/register/util.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include <cstdint>
+#include <utility>
+#include <tuple>
 
 #include "concept.h"
 
@@ -111,5 +113,48 @@ namespace Soc::Reg {
         constexpr std::uint32_t WriteOnly = static_cast<std::uint32_t>(FieldAccessRightsEnum::WriteOnly);
         constexpr std::uint32_t ReadWrite = static_cast<std::uint32_t>(FieldAccessRightsEnum::ReadWrite);
     }
+
+    #pragma region Field Bitmask and Field Access
+
+    template<typename Tuple, std::size_t Index>
+    constexpr auto getBitmaskFromTupleIndex() -> std::uint32_t {
+        return static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldBitMask);
+    }
+
+    template<typename Tuple, std::size_t Index>
+    constexpr auto getWritableFromTupleIndex() -> bool {
+        return Soc::Reg::isFieldAccessWritable<static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldAccess)>();
+    }
+
+    template<typename Tuple, std::size_t... Indices>
+    constexpr auto getWritableMaskFromTupleIndices(std::index_sequence<Indices...>) -> std::uint32_t {
+        return ( (getWritableFromTupleIndex<Tuple, Indices>?getBitmaskFromTupleIndex<Tuple, Indices>():0) | ...);
+    }
+
+    template<typename Tuple>
+    constexpr auto getWritableMaskFromTuple() -> std::uint32_t {
+        return getMasksFromTupleIndices<Tuple>(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+    }
+
+    #pragma endregion
+
+    #pragma region IsSameAsOneFieldFromTuple
+
+    template<typename RegField, typename Tuple, std::size_t Index>
+    constexpr auto IsSameAsOneFieldFromTupleIndex() -> bool {
+        return std::is_same_v<RegField, std::tuple_element_t<Index, Tuple>>;
+    }
+
+    template<typename RegField, typename Tuple, std::size_t... Indices>
+    constexpr auto IsSameAsOneFieldFromTupleIndices(std::index_sequence<Indices...>) -> bool {
+        return ( IsSameAsOneFieldFromTupleIndex<RegField, Tuple, Indices>() || ...);
+    }
+
+    template<typename RegField, typename Tuple>
+    constexpr bool IsSameAsOneFieldFromTuple() {
+        return IsSameAsOneFieldFromTupleIndices<RegField, Tuple>(std::make_index_sequence<std::tuple_size_v<Tuple>>{});
+    }
+
+    #pragma endregion
 
 }
