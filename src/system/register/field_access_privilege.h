9a88df2636b2c668838480bb6e35234e60399365
Anton Krug <antonkrug@antonkrug.eu>
2025-02-17 21:24:29 +0000

    let's not depend on namespaces so deep, and we will be referencing them fully

--- src/system/register/field_access_privilege.h
+++ src/system/register/field_access_privilege.h
@@ -6,23 +6,21 @@
 
 #include <cstdint>
 
-namespace Soc::Reg::FieldAccessPrivilege::Field {
+namespace Soc::Reg::FieldAccessRight {
 
-    enum class AccessRightsEnum: std::uint32_t {
+    enum class AccessRightEnum: std::uint32_t {
         ReadOnly,
         WriteOnly,
         ReadWrite
     };
 
-    namespace AccessRights {
-        constexpr std::uint32_t ReadOnly  = static_cast<std::uint32_t>(AccessRightsEnum::ReadOnly);
-        constexpr std::uint32_t WriteOnly = static_cast<std::uint32_t>(AccessRightsEnum::WriteOnly);
-        constexpr std::uint32_t ReadWrite = static_cast<std::uint32_t>(AccessRightsEnum::ReadWrite);
-    }
+    constexpr std::uint32_t ReadOnly  = static_cast<std::uint32_t>(AccessRightEnum::ReadOnly);
+    constexpr std::uint32_t WriteOnly = static_cast<std::uint32_t>(AccessRightEnum::WriteOnly);
+    constexpr std::uint32_t ReadWrite = static_cast<std::uint32_t>(AccessRightEnum::ReadWrite);
 
-    template<AccessRightsEnum access>
+    template<AccessRightEnum access>
     constexpr auto isWritable() -> bool {
-        if (access == AccessRightsEnum::ReadOnly) {
+        if (access == AccessRightEnum::ReadOnly) {
             return false;
         }
         return true;
@@ -30,7 +28,7 @@ namespace Soc::Reg::FieldAccessPrivilege::Field {
 
     template<std::uint32_t access>
     constexpr auto isWritable() -> bool {
-        if (access == static_cast<std::uint32_t>(AccessRightsEnum::ReadOnly)) {
+        if (access == static_cast<std::uint32_t>(AccessRightEnum::ReadOnly)) {
             return false;
         }
         return true;
