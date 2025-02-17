d390f8b088a8fdcbad8f0d5fb7c6a271be72835a
Anton Krug <antonkrug@antonkrug.eu>
2025-02-17 21:34:48 +0000

    no need for full naspace when we are inside it

--- src/system/register/combine.h
+++ src/system/register/combine.h
@@ -39,7 +39,7 @@ namespace Soc::Reg::Combine {
 
     template<typename Tuple, std::size_t Index>
     constexpr auto writableFromTupleTypeIndex() -> bool {
-        return Soc::Reg::FieldAccessPrivilege::Field::isWritable<static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldAccess)>();
+        return FieldAccessRight::isWritable<static_cast<std::uint32_t>(std::tuple_element_t<Index, Tuple>::fieldAccess)>();
     }
 
     template<typename Tuple, std::size_t... Indices>
