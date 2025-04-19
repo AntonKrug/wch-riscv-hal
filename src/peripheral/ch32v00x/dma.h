062a8fe2ba478f0f3c734614a2547e122d06b8a1
Anton Krug <antonkrug@antonkrug.eu>
2025-04-19 19:05:36 +0100

    DMA triggers added

--- src/peripheral/ch32v00x/dma.h
+++ src/peripheral/ch32v00x/dma.h
@@ -10,7 +10,49 @@
 
 namespace Peripheral::Dma {
 
-    constexpr std::uint32_t baseAddr = 0x4002'0000;
+    constexpr std::uint32_t baseAddr = 0x4002'0000U;
+
+    enum class DmaId : std::uint32_t {
+        // DMA1 ch1 triggers
+        Adc1HwTrigger     = 0x011U,
+        Tim2Ch3HwTrigger  = 0x011U,
+        Dma1Ch1SwTrigger  = 0x111U,
+
+        // DMA1 ch2 triggers
+        Spi1RxHwTrigger   = 0x012U,
+        Tim1Ch1HwTrigger  = 0x012U,
+        Tim2UpHwTrigger   = 0x012U,
+        Dma1Ch2SwTrigger  = 0x112U,
+
+        // DMA1 ch3 triggers
+        Spi1TxHwTrigger   = 0x013U,
+        Tim1Ch2HwTrigger  = 0x013U,
+        Dma1Ch3SwTrigger  = 0x113U,
+
+        // DMA1 ch4 triggers
+        Usart1TxHwTrigger = 0x014U,
+        Tim1Ch4HwTrigger  = 0x014U,
+        Tim1TrigHwTrigger = 0x014U,
+        Tim1ComHwTrigger  = 0x014U,
+        Dma1Ch4SwTrigger  = 0x114U,
+
+        // DMA1 ch5 triggers
+        Usart1RxHwTrigger = 0x015U,
+        Tim1UpHwTrigger   = 0x015U,
+        Tim2Ch1HwTrigger  = 0x015U,
+        Dma1Ch5SwTrigger  = 0x115U,
+
+        // DMA1 ch6 triggers
+        I2c1TxHwTrigger   = 0x016U,
+        Tim1Ch3HwTrigger  = 0x016U,
+        Dma1Ch6SwTrigger  = 0x116U,
+
+        // DMA1 ch7 triggers
+        I2cRxHwTrigger    = 0x017U,
+        Tim2Ch2HwTrigger  = 0x017U,
+        Tim2Ch4HwTrigger  = 0x017U,
+        Dma1Ch7SwTrigger  = 0x117U,
+    };
 
     enum class Direction {
         PeripheralToMemory, // MEM2MEM=0, DIR=0  *MADDR=*PADDR
@@ -32,21 +74,22 @@ namespace Peripheral::Dma {
     };
 
     template<
-        std::uint8_t TplInstance,
-        std::uint8_t TplChannel,
-        Direction TplDirection,
+        std::uint8_t  TplInstance,
+        std::uint8_t  TplChannel,
+        Direction     TplDirection,
         std::uint32_t TplSourceAddress,
-        std::uint32_t TplDestinationAddress,
-        Priority TplPriority,
         SizeAlignment TplSourceAlignment,
+        bool          TplSourceIncrement,
+        std::uint32_t TplDestinationAddress,
         SizeAlignment TplDestinationAlignment,
-        bool TplSourceIncrement,
-        bool TplDestinationIncrement,
-        bool TplCyclicMode,
-        bool TplTransmissionErrorIrq,
-        bool TplHalfTransmissionIrq,
-        bool TplFullTransmissionIrq,
-        bool TplEnableIrq
+        bool          TplDestinationIncrement,
+        std::uint16_t TplSizeOfTransmission,
+        bool          TplCyclicMode,
+        Priority      TplPriority,
+        bool          TplTransmissionErrorIrq,
+        bool          TplHalfTransmissionIrq,
+        bool          TplFullTransmissionIrq,
+        bool          TplEnableIrq
     >
     constexpr static auto configureDma() -> void {
     }
