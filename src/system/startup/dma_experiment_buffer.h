66cb00f8b199edbce7940d749d91e48a0c30d9ea
Anton Krug <antonkrug@antonkrug.eu>
2025-04-20 13:58:26 +0100

    More buffer experiment

--- src/system/startup/dma_experiment_buffer.h
+++ src/system/startup/dma_experiment_buffer.h
@@ -6,11 +6,27 @@
 
 #include <cstdint>
 
-struct Buffer {
-    int value[10];
-};
-
-
-inline constexpr Buffer globalBuffer{42};
-inline constexpr const void* globalBufferPtr = &globalBuffer;
+// struct Buffer {
+//     int value[10];
+// };
+//
+//
+// inline constexpr Buffer globalBuffer{42};
+// inline constexpr const void* globalBufferPtr = &globalBuffer;
 // inline constexpr std::uintptr_t globalBufferAddr = (std::uintptr_t)(globalBufferPtr);
+//
+
+constinit int myVar = 10;
+
+
+constexpr int* getPointer() {
+    return &myVar;  // Return a constexpr pointer to myVar
+}
+
+constexpr uintptr_t getPointerAsUintPtr() {
+    return reinterpret_cast<uintptr_t>(&myVar);  // Convert pointer to uintptr_t
+}
+
+// constexpr std::uintptr_t ptr = (uintptr_t)(static_cast<void *>(getPointer()));
+
+constexpr std::uintptr_t ptr2 = getPointerAsUintPtr();
