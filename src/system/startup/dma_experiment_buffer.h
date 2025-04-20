b6a29d9bc847a89ef2859371d8e92d9ce562910e
Anton Krug <antonkrug@antonkrug.eu>
2025-04-20 22:52:16 +0100

    constexpr buffer pointers temporary removed

--- src/system/startup/dma_experiment_buffer.h
+++ src/system/startup/dma_experiment_buffer.h
@@ -16,17 +16,17 @@
 // inline constexpr std::uintptr_t globalBufferAddr = (std::uintptr_t)(globalBufferPtr);
 //
 
-constinit int myVar = 10;
-
-
-constexpr int* getPointer() {
-    return &myVar;  // Return a constexpr pointer to myVar
-}
-
-constexpr uintptr_t getPointerAsUintPtr() {
-    return reinterpret_cast<uintptr_t>(&myVar);  // Convert pointer to uintptr_t
-}
-
-// constexpr std::uintptr_t ptr = (uintptr_t)(static_cast<void *>(getPointer()));
-
-constexpr std::uintptr_t ptr2 = getPointerAsUintPtr();
+// constinit int myVar = 10;
+//
+//
+// constexpr int* getPointer() {
+//     return &myVar;  // Return a constexpr pointer to myVar
+// }
+//
+// constexpr uintptr_t getPointerAsUintPtr() {
+//     return reinterpret_cast<uintptr_t>(&myVar);  // Convert pointer to uintptr_t
+// }
+//
+// // constexpr std::uintptr_t ptr = (uintptr_t)(static_cast<void *>(getPointer()));
+//
+// constexpr std::uintptr_t ptr2 = getPointerAsUintPtr();
