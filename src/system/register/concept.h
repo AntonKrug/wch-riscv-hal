fac389434fe65ee95d71d55e0c744971e53ede20
Anton Krug <antonkrug@antonkrug.eu>
2025-04-21 15:33:53 +0100

    With not Whith, added enable/disable/offset concepts, added comments

--- src/system/register/concept.h
+++ src/system/register/concept.h
@@ -6,36 +6,77 @@
 
 namespace Soc::Reg::Concept {
 
-    #pragma region FieldType
+    #pragma region FieldType - concepts checking various properties of the Register Field Enums
 
     template<typename RegFieldType>
     concept FieldTypeWithBitMask = requires
         { { RegFieldType::fieldBitMask }; };
 
-
     template<typename RegFieldType>
-    concept FieldTypeWhithAccess = requires
+    concept FieldTypeWithAccess = requires
         { { RegFieldType::fieldAccess }; };
 
     template<typename RegFieldType>
-    concept FieldTypeWhithMaskAndAccess =
-        FieldTypeWithBitMask<RegFieldType> and FieldTypeWhithAccess<RegFieldType>;
+    concept FieldTypeWithMaskAndAccess =
+        FieldTypeWithBitMask<RegFieldType> and FieldTypeWithAccess<RegFieldType>;
+
+    template<typename RegFieldType>
+    concept FieldTypeWithEnableValue = requires
+    { { RegFieldType::Enable }; };
+
+    template<typename RegFieldType>
+    concept FieldTypeWithDisableValue = requires
+    { { RegFieldType::Disable }; };
+
+    template<typename RegFieldType>
+    concept FieldTypeWithFieldBitOffset = requires
+    { { RegFieldType::fieldBitOffset }; };
+
+    template<typename RegFieldType>
+    concept FieldTypeWithEnableAndDisableValues =
+        FieldTypeWithEnableValue<RegFieldType> and FieldTypeWithDisableValue<RegFieldType>;
+
+    // Supports bool semantics (has enable/disable) values and has offset which can be used for conversions
+    // TODO: check size of the bitmask and allow 1bit enums
+    template<typename RegFieldType>
+    concept FieldTypeWithBoolInteroperability =
+        FieldTypeWithEnableValue<RegFieldType> and FieldTypeWithDisableValue<RegFieldType> and FieldTypeWithFieldBitOffset<RegFieldType>;
 
     #pragma endregion
 
-    #pragma region Field
+    #pragma region Field - helpers so we can use FieldType concepts without having to get the type with decltype
 
-    template<auto RegField>
+    template<auto TplRegField>
     concept FieldWithBitMask =
-    FieldTypeWithBitMask<decltype(RegField)>;
+    FieldTypeWithBitMask<decltype(TplRegField)>;
 
-    template<auto RegField>
-    concept FieldWhithAccess =
-        FieldTypeWhithAccess<decltype(RegField)>;
+    template<auto TplRegField>
+    concept FieldWithAccess =
+        FieldTypeWithAccess<decltype(TplRegField)>;
 
-    template<auto RegField>
-    concept FieldWhithMaskAndAccess =
-        FieldWithBitMask<RegField> and FieldWhithAccess<RegField>;
+    template<auto TplRegField>
+    concept FieldWithMaskAndAccess =
+        FieldTypeWithMaskAndAccess<decltype(TplRegField)>;
+
+    template<auto TplRegField>
+    concept FieldWithEnableValue =
+        FieldTypeWithEnableValue<decltype(TplRegField)>;
+
+    template<auto TplRegField>
+    concept FieldWithDisableValue =
+        FieldTypeWithDisableValue<decltype(TplRegField)>;
+
+    template<auto TplRegField>
+    concept FieldWithFieldBitOffset =
+        FieldTypeWithFieldBitOffset<decltype(TplRegField)>;
+
+    template<auto TplRegField>
+    concept FieldWithEnableAndDisableValues =
+        FieldTypeWithEnableAndDisableValues<decltype(TplRegField)>;
+
+    template<auto TplRegField>
+    concept FieldWithBoolInteroperability =
+        FieldTypeWithBoolInteroperability<decltype(TplRegField)>;
 
     #pragma endregion
 
