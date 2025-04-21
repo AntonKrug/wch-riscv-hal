7a62752e20597d0e190fdb1090b7200dbe5197e6
Anton Krug <antonkrug@antonkrug.eu>
2025-04-21 22:38:05 +0100

    Template name syntax

--- src/system/register/access_primitives_ct.h
+++ src/system/register/access_primitives_ct.h
@@ -8,34 +8,34 @@
 
 namespace Soc::Reg::Access {
 
-    template<std::uint32_t Addr>
+    template<std::uint32_t TplAddr>
     inline auto
     __attribute__ ((
         always_inline,
         optimize("-Os"),
     ))
     readCt() -> std::uint32_t {
-        return *reinterpret_cast<volatile std::uint32_t *>(Addr);
+        return *reinterpret_cast<volatile std::uint32_t *>(TplAddr);
     }
 
-    template<std::uint32_t Addr, std::uint32_t CtValue>
+    template<std::uint32_t TplAddr, std::uint32_t TplCtValue>
     inline auto
     __attribute__ ((
         always_inline,
         optimize("-Os"),
     ))
     writeCt() -> void {
-        *(reinterpret_cast<volatile std::uint32_t *>(Addr))=CtValue;
+        *(reinterpret_cast<volatile std::uint32_t *>(TplAddr))=TplCtValue;
     }
 
-    template<std::uint32_t Addr>
+    template<std::uint32_t TplAddr>
     inline auto
     __attribute__ ((
         always_inline,
         optimize("-Os"),
     ))
     writeCt(const std::uint32_t rtValue) -> void {
-        *(reinterpret_cast<volatile std::uint32_t *>(Addr))=rtValue;
+        *(reinterpret_cast<volatile std::uint32_t *>(TplAddr))=rtValue;
     }
 
 }
