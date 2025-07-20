1b2bee5f4eeb4a86f00d566675118a5f5fb909c6
Anton Krug <antonkrug@antonkrug.eu>
2025-07-20 20:09:38 +0100

    start GDB with prefered script/settings

---
+++ launch/real-hw/gdb-start.cmd
@@ -0,0 +1,5 @@
+REM can't use the `set disassembler-options xw` with upstream xpack toolchain, but can have TUI mode
+REM and can't access the CSR intsyscr as it's just 0x804
+riscv-none-elf-gdb.exe -tui -x gdb/hw_script ../../cmake-build-debug/src/wch-riscv-hal-pass-1.hex
+
+REM riscv-none-elf-gdb.exe -x gdb/hw_script ../../cmake-build-debug/src/wch-riscv-hal-pass-1.hex
