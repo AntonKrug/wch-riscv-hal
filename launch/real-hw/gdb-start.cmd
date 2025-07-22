bcfbeb7fb67ae0ef17c90537bbba818a8da56b81
Anton Krug <antonkrug@antonkrug.eu>
2025-07-22 01:18:25 +0100

    actually both WCH and xpack have python support

--- launch/real-hw/gdb-start.cmd
+++ launch/real-hw/gdb-start.cmd
@@ -1,5 +1,11 @@
 REM can't use the `set disassembler-options xw` with upstream xpack toolchain, but can have TUI mode
 REM and can't access the CSR intsyscr as it's just 0x804
-riscv-none-elf-gdb.exe -tui -x gdb/gdbinit ../../cmake-build-debug/src/wch-riscv-hal-pass-1.elf
+
+REM xpack toolchain
+REM riscv-none-elf-gdb.exe -tui -x gdb/gdbinit ../../cmake-build-debug/src/wch-riscv-hal-pass-1.elf
+riscv-none-elf-gdb-py3.exe -tui -x gdb/gdbinit ../../cmake-build-debug/src/wch-riscv-hal-pass-1.elf
+
+REM WCH toolchain
+REM riscv-wch-elf-gdb-py3.exe -x gdb/gdbinit ../../cmake-build-debug/src/wch-riscv-hal-pass-1.elf
 
 REM riscv-none-elf-gdb.exe -x gdb/hw_script ../../cmake-build-debug/src/wch-riscv-hal-pass-1.hex
